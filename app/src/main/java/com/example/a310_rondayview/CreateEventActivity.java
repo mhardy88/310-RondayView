31200989b0478196c568e5a9c6a8df72d78bd9ca
Michael Hardy <mhar860@aucklanduni.ac.nz>
2023-08-12 20:50:31 +1200

    image upload works

--- app/src/main/java/com/example/a310_rondayview/CreateEventActivity.java
+++ app/src/main/java/com/example/a310_rondayview/CreateEventActivity.java
@@ -1,14 +1,60 @@
 package com.example.a310_rondayview;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.CursorJoiner;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    private class ViewHolder {
+        Button choose_image_btn;
+        ImageView event_image_img;
+
+        public ViewHolder() {
+            choose_image_btn = findViewById(R.id.choose_image_btn);
+            event_image_img = findViewById(R.id.event_image_img);
+        }
+    }
+
+    ViewHolder vh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        vh = new ViewHolder();
+
+        vh.choose_image_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(selectImageIntent, 3);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        vh = new ViewHolder();
+        if(resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            vh.event_image_img.setImageURI(selectedImage);
+        }
+    }
+
 }
--- app/src/main/java/com/example/a310_rondayview/MainActivity.java
+++ app/src/main/java/com/example/a310_rondayview/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.a310_rondayview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private class ViewHolder {
+
+        Button change_page;
+
+        public ViewHolder() {
+            change_page = findViewById(R.id.change_page);
+        }
+    }
+
+    ViewHolder vh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vh = new ViewHolder();
+
+        vh.change_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changePageIntent = new Intent(getBaseContext(), CreateEventActivity.class);
+                startActivity(changePageIntent);
+            }
+        });
     }
+
+
 }
